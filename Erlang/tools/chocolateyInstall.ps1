a417dab02fc39a08c08e9279146c1e858b2c4c3b
Onorio Catenacci <ocatenacci@unitedshore.com>
2016-09-22 08:15:43 -0400

    Modifications to make erlang install silent and to add checksums to the package

--- Erlang/tools/chocolateyInstall.ps1
+++ Erlang/tools/chocolateyInstall.ps1
@@ -1,8 +1,30 @@
-﻿$package = 'erlang'
+﻿<#
+.SYNOPSIS
+An install script for installing Erlang silently on the machine via ChocolateyNuGet
+
+.NOTES
+Author: Onorio Catenacci - catenacci@ieee.org
+Version: 19.0.20160917
+#>
+
+$package = 'erlang'
 $version = '19.0'
 $erl_version = '8.0'
 
-Install-ChocolateyPackage $package 'EXE' '/S' http://www.erlang.org/download/otp_win32_$version.exe http://www.erlang.org/download/otp_win64_$version.exe  -validExitCodes @(0)
+$params = @{
+  PackageName = $package
+  FileType = 'exe'
+  SilentArgs = '/S'
+  Url = "http://www.erlang.org/download/otp_win32_$version.exe"
+  CheckSum = '62ef79ef798434a6ad5828d2ad275d9c26d8a1c638db6860d2325c3e00b8a803'
+  CheckSumType = 'sha256'
+  Url64 = "http://www.erlang.org/download/otp_win64_$version.exe"
+  CheckSum64 = 'ab1811996cac1bff1248dbd3d840b012c9585ea6ee746862aa297e598a35ad3c'
+  CheckSumType64 = 'sha256'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @params
 
 
 Generate-BinFile "erl" -path "$env:ProgramFiles/erl$erl_version/erts-$erl_version/bin/erl.exe"
