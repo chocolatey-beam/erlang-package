4338c82ab3b26572a33f4fd74fdac414d3df9c6f
Onorio Catenacci <catenacci@ieee.org>
2017-06-01 21:51:49 -0400

    Updating Rebar
    Updating Rebar3 to 3.4.1

--- Rebar/tools/chocolateyInstall.ps1
+++ Rebar/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$package = 'rebar3'
-$version = '3.3.5'
+$version = '3.4.1'
 
 $toolsDir = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
 Get-ChocolateyWebFile -packageName $package -fileFullPath $toolsDir/$package -url "https://github.com/erlang/$package/releases/download/$version/rebar3"
--- Rebar/tools/chocolateyUninstall.ps1
+++ Rebar/tools/chocolateyUninstall.ps1
@@ -1,5 +1,5 @@
 ﻿$package = 'rebar3'
-$version = '3.3.5'
+$version = '3.4.1'
 
 #Note that this process will _not_ uninstall Erlang.
 #Remove the shim file.
