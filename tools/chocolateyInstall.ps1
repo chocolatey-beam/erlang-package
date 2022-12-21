d0c64760055ccb0d62c70d7dfb4c9614e20fbad7
Luke Bakken <luke@bakken.io>
2022-12-21 10:59:01 -0800

    Update for Erlang 25.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,35 +3,39 @@
 An install script for installing Erlang silently on the machine via ChocolateyNuGet
 
 .NOTES
-Author: Onorio Catenacci - catenacci@ieee.org
-Version: 24.0
+Author: Luke Bakken - luke@bakken.io
+Version: 25.2
 #>
 
-$package = 'erlang'
-$version = '24.0'
-$erl_version = '12.0'
+New-Variable -Name package -Value 'erlang' -Option Constant
+New-Variable -Name otp_version -Value '25.2' -Option Constant
+New-Variable -Name erts_version -Value '13.1.3' -Option Constant
 
 $params = @{
   PackageName = $package
   FileType = 'exe'
   SilentArgs = '/S'
-  Url = "https://erlang.org/download/otp_win32_$version.exe"
-  CheckSum = 'ccc1e5918aefb543d2b5e7547c44e1b1ff66d62cd4ea74dd4782f694a6de8a44'
+  Url = "https://github.com/erlang/otp/releases/download/OTP-$otp_version/otp_win32_$otp_version.exe"
+  CheckSum = '4d9b142556b69d4ccc449e2b79248a736c8e6e02b400cccb1656a8230427b680'
   CheckSumType = 'sha256'
-  Url64 = "https://erlang.org/download/otp_win64_$version.exe"
-  CheckSum64 = 'f13311ae26d5260566740f8a7f124d0ba3589a1f52aada84b236825641f53225'
+  Url64 = "https://github.com/erlang/otp/releases/download/OTP-$otp_version/otp_win64_$otp_version.exe"
+  CheckSum64 = '412eec08acf8b3f0305e9a16efd57ab901e30c1c24379c171cefc74fadc301c2'
   CheckSumType64 = 'sha256'
   validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @params
 
-$baseErlangPath = "$env:ProgramFiles\erl-$version\erts-$erl_version\bin"
+New-Variable -Name erlangProgramFilesPath -Option Constant`
+    -Value ((Get-ItemProperty -Path HKCU:\Software\Ericsson\Erlang\$erts_version).'(default)')
 
-Generate-BinFile "ct_run" -path "$baseErlangPath\ct_run.exe"
-Generate-BinFile "erl" -path "$baseErlangPath\erl.exe"
-Generate-BinFile "werl" -path "$baseErlangPath\werl.exe"
-Generate-BinFile "erlc" -path "$baseErlangPath\erlc.exe"
-Generate-BinFile "escript" -path "$baseErlangPath\escript.exe"
-Generate-BinFile "dialyzer" -path "$baseErlangPath\dialyzer.exe"
-Generate-BinFile "typer" -path "$baseErlangPath\typer.exe"
+New-Variable -Name erlangErtsBinPath -Option Constant `
+    -Value (Join-Path -Path $erlangProgramFilesPath -ChildPath "erts-$erts_version" | Join-Path -ChildPath 'bin')
+
+Install-BinFile -Name 'ct_run'   -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'ct_run.exe')
+Install-BinFile -Name 'erl'      -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'erl.exe')
+Install-BinFile -Name 'werl'     -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'werl.exe')
+Install-BinFile -Name 'erlc'     -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'erlc.exe')
+Install-BinFile -Name 'escript'  -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'escript.exe')
+Install-BinFile -Name 'dialyzer' -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'dialyzer.exe')
+Install-BinFile -Name 'typer'    -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'typer.exe')
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,17 +1,30 @@
-﻿$package = 'erlang'
-$version = '24.0'
-$erl_version = '12.0'
+﻿<#
+.SYNOPSIS
+An uninstall script for Erlang
 
-Start-Process -Wait "$env:ProgramFiles\erl-$version\uninstall.exe" -ArgumentList "/S"
+.NOTES
+Author: Luke Bakken - luke@bakken.io
+Version: 25.2
+#>
 
-#And remove the shim files as well.
-$baseErlangPath = "$env:ProgramFiles\erl-$version\erts-$erl_version\bin"
+New-Variable -Name package -Value 'erlang' -Option Constant
+New-Variable -Name otp_version -Value '25.2' -Option Constant
+New-Variable -Name erts_version -Value '13.1.3' -Option Constant
 
-Remove-BinFile "ct_run" -path "$baseErlangPath\ct_run.exe"
-Remove-BinFile "erl" -path "$baseErlangPath\erl.exe"
-Remove-BinFile "werl" -path "$baseErlangPath\werl.exe"
-Remove-BinFile "erlc" -path "$baseErlangPath\erlc.exe"
-Remove-BinFile "escript" -path "$baseErlangPath\escript.exe"
-Remove-BinFile "dialyzer" -path "$baseErlangPath\dialyzer.exe"
-Remove-BinFile "typer" -path "$baseErlangPath\typer.exe"
+New-Variable -Name erlangProgramFilesPath -Option Constant `
+    -Value ((Get-ItemProperty -Path HKCU:\Software\Ericsson\Erlang\$erts_version).'(default)')
 
+Start-Process -Wait -FilePath (Join-Path -Path $erlangProgramFilesPath -ChildPath 'uninstall.exe') -ArgumentList '/S'
+
+# ...and remove the shim files as well.
+
+New-Variable -Name erlangErtsBinPath -Option Constant `
+    -Value (Join-Path -Path $erlangProgramFilesPath -ChildPath "erts-$erts_version" | Join-Path -ChildPath 'bin')
+
+Uninstall-BinFile -Name 'ct_run'   -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'ct_run.exe')
+Uninstall-BinFile -Name 'erl'      -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'erl.exe')
+Uninstall-BinFile -Name 'werl'     -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'werl.exe')
+Uninstall-BinFile -Name 'erlc'     -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'erlc.exe')
+Uninstall-BinFile -Name 'escript'  -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'escript.exe')
+Uninstall-BinFile -Name 'dialyzer' -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'dialyzer.exe')
+Uninstall-BinFile -Name 'typer'    -Path (Join-Path -Path $erlangErtsBinPath -ChildPath 'typer.exe')
