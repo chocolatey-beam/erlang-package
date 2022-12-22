18cbb8c4bdd710e9de15f15676c883bac6fb2c76
Luke Bakken <luke@bakken.io>
2022-12-21 18:38:03 -0800

    Templatize scripts and finish up template script

--- package.ps1
+++ package.ps1
@@ -1,31 +1,123 @@
 $DebugPreference = "Continue"
 $ErrorActionPreference = 'Stop'
-Set-PSDebug -Strict -Trace 1
+# Set-PSDebug -Strict -Trace 1
+Set-PSDebug -Off
 Set-StrictMode -Version 'Latest' -ErrorAction 'Stop' -Verbose
 
-New-Variable -Name ej -Option Constant `
-    -Value (Invoke-WebRequest -Uri https://api.github.com/repos/erlang/otp/releases/latest | ConvertFrom-Json)
+New-Variable -Name curdir  -Option Constant -Value $PSScriptRoot
 
-New-Variable -Name otp_version -Option Constant -Value $ej.tag_name
+Write-Host "[INFO] curdir: $curdir"
+
+try
+{
+    $ProgressPreference = 'SilentlyContinue'
+    New-Variable -Name ej -Option Constant `
+        -Value (Invoke-WebRequest -Uri https://api.github.com/repos/erlang/otp/releases/latest | ConvertFrom-Json)
+}
+finally
+{
+    $ProgressPreference = 'Continue'
+}
+
+New-Variable -Name otp_version -Option Constant `
+    -Value ($ej.tag_name -replace '^OTP-', '')
 
 Write-Host "[INFO] otp_version: $otp_version"
 
-New-Variable -Name installer_asset  -Option Constant `
+New-Variable -Name win32_installer_asset  -Option Constant `
+    -Value ($ej.assets | Where-Object { $_.name -match '^otp_win32_[0-9.]+\.exe$' })
+New-Variable -Name win64_installer_asset  -Option Constant `
     -Value ($ej.assets | Where-Object { $_.name -match '^otp_win64_[0-9.]+\.exe$' })
 
-New-Variable -Name installer_exe -Option Constant -Value $installer_asset.name
+New-Variable -Name win32_installer_exe -Option Constant -Value $win32_installer_asset.name
+New-Variable -Name win64_installer_exe -Option Constant -Value $win64_installer_asset.name
 
-if (!(Test-Path -Path $installer_exe))
+$files = @()
+$jobs = @()
+
+if (!(Test-Path -Path $win32_installer_exe))
 {
-    Invoke-WebRequest -Uri $installer_asset.browser_download_url -OutFile $installer_exe
+    $files += @{
+        Uri = $win32_installer_asset.browser_download_url
+        OutFile = $win32_installer_exe
+    }
+}
+if (!(Test-Path -Path $win64_installer_exe))
+{
+    $files += @{
+        Uri = $win64_installer_asset.browser_download_url
+        OutFile = $win64_installer_exe
+    }
 }
 
-New-Variable -Name installer_exe_sha256 -Option Constant `
-    -Value (Get-FileHash -Path $installer_exe -Algorithm SHA256).Hash.ToLowerInvariant()
+try
+{
+    $ProgressPreference = 'SilentlyContinue'
+    foreach ($file in $files) {
+        $jobs += Start-ThreadJob -Name $file.OutFile -ScriptBlock {
+            $params = $using:file
+            Invoke-WebRequest @params
+        }
+    }
 
-Start-Process -Wait -FilePath $installer_exe -ArgumentList '/S'
+    if ($jobs.Length > 0)
+    {
+        Write-Host "[INFO] Downloads started..."
+        Wait-Job -Job $jobs
+        foreach ($job in $jobs)
+        {
+            Receive-Job -Job $job
+        }
+        Write-Host "[INFO] Downloads complete!"
+    }
+}
+finally
+{
+    $ProgressPreference = 'Continue'
+}
+
+New-Variable -Name win32_installer_exe_sha256 -Option Constant `
+    -Value (Get-FileHash -Path $win32_installer_exe -Algorithm SHA256).Hash.ToLowerInvariant()
+New-Variable -Name win64_installer_exe_sha256 -Option Constant `
+    -Value (Get-FileHash -Path $win64_installer_exe -Algorithm SHA256).Hash.ToLowerInvariant()
+
+Write-Host "[INFO] win32 installer sha256: $win32_installer_exe_sha256"
+Write-Host "[INFO] win64 installer sha256: $win64_installer_exe_sha256"
+
+Write-Host "[INFO] installing Erlang..."
+Start-Process -Wait -FilePath $win64_installer_exe -ArgumentList '/S'
+Write-Host "[INFO] installation complete!"
 
 New-Variable -Name erts_version -Option Constant `
     -Value (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Ericsson\Erlang | Select-Object -Last 1).PSChildName
 
 Write-Host "[INFO] erts_version: $erts_version"
+
+(Get-Content -Raw -Path erlang.nuspec.in).Replace('@@OTP_VERSION@@', $otp_version) | Set-Content erlang.nuspec
+
+# install
+New-Variable -Name chocolateyInstallPs1In -Option Constant `
+    -Value (Join-Path -Path $curdir -ChildPath 'tools' | Join-Path -ChildPath 'chocolateyInstall.ps1.in')
+
+New-Variable -Name chocolateyInstallPs1 -Option Constant `
+    -Value (Join-Path -Path $curdir -ChildPath 'tools' | Join-Path -ChildPath 'chocolateyInstall.ps1')
+
+(Get-Content -Raw -Path $chocolateyInstallPs1In).Replace('@@OTP_VERSION@@', $otp_version).Replace('@@ERTS_VERSION@@', $erts_version).Replace('@@WIN32_SHA256@@', $win32_installer_exe_sha256).Replace('@@WIN64_SHA256@@', $win64_installer_exe_sha256) | Set-Content $chocolateyInstallPs1
+
+# uninstall
+New-Variable -Name chocolateyUninstallPs1In -Option Constant `
+    -Value (Join-Path -Path $curdir -ChildPath 'tools' | Join-Path -ChildPath 'chocolateyUninstall.ps1.in')
+
+New-Variable -Name chocolateyUninstallPs1 -Option Constant `
+    -Value (Join-Path -Path $curdir -ChildPath 'tools' | Join-Path -ChildPath 'chocolateyUninstall.ps1')
+
+(Get-Content -Raw -Path $chocolateyUninstallPs1In).Replace('@@OTP_VERSION@@', $otp_version).Replace('@@ERTS_VERSION@@', $erts_version) | Set-Content $chocolateyUninstallPs1
+
+New-Variable -Name erlangProgramFilesPath -Option Constant `
+    -Value ((Get-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Ericsson\Erlang\$erts_version).'(default)')
+
+Write-Host "[INFO] UN-installing Erlang..."
+Start-Process -Wait -FilePath (Join-Path -Path $erlangProgramFilesPath -ChildPath 'uninstall.exe') -ArgumentList '/S'
+Write-Host "[INFO] uninstallation complete!"
+
+Set-PSDebug -Off
